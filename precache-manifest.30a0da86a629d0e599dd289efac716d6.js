db846cf3ac165f63c5f5e46fe77603d358a0404a
newscutter <59840573+newscutter@users.noreply.github.com>
2020-02-28 11:23:54 -0500

    Updates

---
+++ precache-manifest.30a0da86a629d0e599dd289efac716d6.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d969c3ae6295af1fc40e58d466c20c",
+    "url": "/haven3.0/index.html"
+  },
+  {
+    "revision": "4891ab1034196b851ac6",
+    "url": "/haven3.0/static/css/2.8e866998.chunk.css"
+  },
+  {
+    "revision": "28293febba289a00c0b1",
+    "url": "/haven3.0/static/js/0.45cc361e.chunk.js"
+  },
+  {
+    "revision": "5054ac8328797d4a27e3914719f138be",
+    "url": "/haven3.0/static/js/0.45cc361e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d786fb6b8d5e4d8d962",
+    "url": "/haven3.0/static/js/1.3be38e46.chunk.js"
+  },
+  {
+    "revision": "cfeb64ecfcd6f1d4102c",
+    "url": "/haven3.0/static/js/10.551a63a9.chunk.js"
+  },
+  {
+    "revision": "7f047a9be875ad2e6fab",
+    "url": "/haven3.0/static/js/11.09f0ce79.chunk.js"
+  },
+  {
+    "revision": "50aaa730ace776c48be5",
+    "url": "/haven3.0/static/js/12.5767dc1d.chunk.js"
+  },
+  {
+    "revision": "8540dc498fdd6e06584f",
+    "url": "/haven3.0/static/js/13.8675acae.chunk.js"
+  },
+  {
+    "revision": "4891ab1034196b851ac6",
+    "url": "/haven3.0/static/js/2.61c272cd.chunk.js"
+  },
+  {
+    "revision": "5af3c7e59191c327e68b",
+    "url": "/haven3.0/static/js/3.7bc484dd.chunk.js"
+  },
+  {
+    "revision": "c307d458d54c9e1524984b0ae1f3e612",
+    "url": "/haven3.0/static/js/3.7bc484dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8098671fb8170dd6414",
+    "url": "/haven3.0/static/js/6.62c4cd7b.chunk.js"
+  },
+  {
+    "revision": "559b2c9bb68aab606f69",
+    "url": "/haven3.0/static/js/7.79278388.chunk.js"
+  },
+  {
+    "revision": "7afcd8ee94cfd094925b",
+    "url": "/haven3.0/static/js/8.c2dd66f1.chunk.js"
+  },
+  {
+    "revision": "97f7fab90ae06f274662",
+    "url": "/haven3.0/static/js/9.9b7886d0.chunk.js"
+  },
+  {
+    "revision": "9d508b80244fdb5c67a1",
+    "url": "/haven3.0/static/js/main.83fe98cc.chunk.js"
+  },
+  {
+    "revision": "57747f116fe14ba0f1b6",
+    "url": "/haven3.0/static/js/runtime-main.d28bdcd3.js"
+  },
+  {
+    "revision": "741dc4163a97ffa354979f18802f6ca8",
+    "url": "/haven3.0/static/media/Inter-Bold.741dc416.woff2"
+  },
+  {
+    "revision": "895ddea987172f5a34a727cb0b559c9d",
+    "url": "/haven3.0/static/media/Inter-Bold.895ddea9.woff"
+  },
+  {
+    "revision": "41cd7069d7f578a69690178b818c0a2c",
+    "url": "/haven3.0/static/media/Inter-Regular.41cd7069.woff"
+  },
+  {
+    "revision": "77812e2fead777db0dc806b1eaabc7fd",
+    "url": "/haven3.0/static/media/Inter-Regular.77812e2f.woff2"
+  },
+  {
+    "revision": "3d011804596fb2ccde4af51830a9a7d5",
+    "url": "/haven3.0/static/media/Inter-SemiBold.3d011804.woff2"
+  },
+  {
+    "revision": "8a12c702fa5c5e3ceed33f78e72b9bee",
+    "url": "/haven3.0/static/media/Inter-SemiBold.8a12c702.woff"
+  },
+  {
+    "revision": "b0abfedd6074dc3fc9eb8e2ba407c3b8",
+    "url": "/haven3.0/static/media/ahawk.b0abfedd.png"
+  },
+  {
+    "revision": "15760d3407cfc4396d1c022b532b1ac9",
+    "url": "/haven3.0/static/media/b_v1.15760d34.svg"
+  },
+  {
+    "revision": "edbc76fc11d05aacaea032f61663cc0c",
+    "url": "/haven3.0/static/media/b_v2.edbc76fc.svg"
+  },
+  {
+    "revision": "ea94a3463b7ae85756ea358f5650c36b",
+    "url": "/haven3.0/static/media/b_v3.ea94a346.svg"
+  },
+  {
+    "revision": "4240d2897ecc731ec464564184be8466",
+    "url": "/haven3.0/static/media/b_v4.4240d289.svg"
+  },
+  {
+    "revision": "16b7d6299db74fa999b2214570152aad",
+    "url": "/haven3.0/static/media/b_v5.16b7d629.svg"
+  },
+  {
+    "revision": "98fbd690a995248729a760cd110469e4",
+    "url": "/haven3.0/static/media/binary.98fbd690.svg"
+  },
+  {
+    "revision": "48b475b83a18625d3cbde520e2ed056c",
+    "url": "/haven3.0/static/media/bitcoin.48b475b8.svg"
+  },
+  {
+    "revision": "44234a7eb38f314ed334166ae2a26880",
+    "url": "/haven3.0/static/media/comingsoon.44234a7e.png"
+  },
+  {
+    "revision": "3925887f8c71abdfcfc0554d1c0e097e",
+    "url": "/haven3.0/static/media/confirmed.3925887f.png"
+  },
+  {
+    "revision": "033f79944ce33a37b5816038e58a89f6",
+    "url": "/haven3.0/static/media/dweab.033f7994.png"
+  },
+  {
+    "revision": "ea53fb224b539c682c06451d7ede2368",
+    "url": "/haven3.0/static/media/ethereum.ea53fb22.svg"
+  },
+  {
+    "revision": "0d0a642a82b23ef43d67efce7e252a35",
+    "url": "/haven3.0/static/media/exchangeconfirmation.0d0a642a.png"
+  },
+  {
+    "revision": "c1c2fd5278df0f40c8a0a6da920baa59",
+    "url": "/haven3.0/static/media/goshiz.c1c2fd52.png"
+  },
+  {
+    "revision": "27bdc9ef91f466f03c09ce4e34ce2f4f",
+    "url": "/haven3.0/static/media/haven.27bdc9ef.svg"
+  },
+  {
+    "revision": "8927c77ac4dce912f920f9cff0c2b2ff",
+    "url": "/haven3.0/static/media/history.8927c77a.svg"
+  },
+  {
+    "revision": "68271a429a7e21340a10f47d7d39f27a",
+    "url": "/haven3.0/static/media/incomplete_dark.68271a42.svg"
+  },
+  {
+    "revision": "4b157b15bb02274878bf17aca39dd0d9",
+    "url": "/haven3.0/static/media/jriggs.4b157b15.png"
+  },
+  {
+    "revision": "636bf7fd4aab1ea98b70ce29bb7caac2",
+    "url": "/haven3.0/static/media/luge.636bf7fd.png"
+  },
+  {
+    "revision": "9ba12c1913a756a4d87460e02a2add34",
+    "url": "/haven3.0/static/media/mad.9ba12c19.png"
+  },
+  {
+    "revision": "c91fcc94890c63522f351996a81c56f9",
+    "url": "/haven3.0/static/media/marty.c91fcc94.png"
+  },
+  {
+    "revision": "b08930ba744490ed02ae12f4a1404d2b",
+    "url": "/haven3.0/static/media/monero.b08930ba.svg"
+  },
+  {
+    "revision": "2d5398dbcb2ce0c8532386f05175b14c",
+    "url": "/haven3.0/static/media/neac.2d5398db.png"
+  },
+  {
+    "revision": "b2448a2c1e0cd1cf19f23a2a88bde6d5",
+    "url": "/haven3.0/static/media/no_transactions.b2448a2c.svg"
+  },
+  {
+    "revision": "7165e7243a79b2235e92c25c949e1a10",
+    "url": "/haven3.0/static/media/pierre.7165e724.png"
+  },
+  {
+    "revision": "a3719dba6724327b006580ed00c32ac0",
+    "url": "/haven3.0/static/media/rare.a3719dba.png"
+  },
+  {
+    "revision": "4164bc8ae96574958b574b606d3d5a55",
+    "url": "/haven3.0/static/media/rounded.4164bc8a.svg"
+  },
+  {
+    "revision": "17d2d05c282ce4c4bbc70aa532ccbc43",
+    "url": "/haven3.0/static/media/settings.17d2d05c.svg"
+  },
+  {
+    "revision": "cc1cabc0f6b970c4e64e0ed853115866",
+    "url": "/haven3.0/static/media/tick.cc1cabc0.svg"
+  },
+  {
+    "revision": "423a0821d00f87d78babbf7c589823a5",
+    "url": "/haven3.0/static/media/vault.423a0821.svg"
+  },
+  {
+    "revision": "fd6e554870b87edf4866fc3c3f1c144e",
+    "url": "/haven3.0/static/media/wp_chinese.fd6e5548.svg"
+  },
+  {
+    "revision": "39115d8e7cb8feb7eb759a0ac9ddc660",
+    "url": "/haven3.0/static/media/wp_english.39115d8e.svg"
+  },
+  {
+    "revision": "768d8f61efc8038a602810e1dcd4f675",
+    "url": "/haven3.0/static/media/wp_french.768d8f61.svg"
+  },
+  {
+    "revision": "337ac1e066a0c7ade1e0d16b1377c865",
+    "url": "/haven3.0/static/media/wp_spanish.337ac1e0.svg"
+  },
+  {
+    "revision": "65bfec75aa36d2bb9582650a42757c9c",
+    "url": "/haven3.0/static/media/wp_vietnamese.65bfec75.svg"
+  }
+]);
